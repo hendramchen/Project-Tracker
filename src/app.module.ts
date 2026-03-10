02863043d1eebd5836957f921fe9188bc0d797e7
Hendra Putu <mchen.putu@gmail.com>
2026-03-10 14:51:10 +0800

    chore: define folder structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { EmployeesModule } from './employees/employees.module';
+import { SkillsModule } from './skills/skills.module';
+import { ProjectsModule } from './projects/projects.module';
+import { ClientsModule } from './clients/clients.module';
+import { SprintsModule } from './sprints/sprints.module';
+import { TasksModule } from './tasks/tasks.module';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    EmployeesModule,
+    SkillsModule,
+    ProjectsModule,
+    ClientsModule,
+    SprintsModule,
+    TasksModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
